5edd367f5c5b5b8a27ea90eeace7d1d6db0c0327
Marcin Król <marcins1024@gmail.com>
2017-06-27 10:55:16 +0200

    Add Shop class

---
+++ shoptask/src/main/java/sages/bootcamp/Shop.java
@@ -0,0 +1,64 @@
+package sages.bootcamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Shop {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  private String name;
+
+  private String address;
+
+  public Shop() {
+  }
+
+  public Shop(int id, String name, String address) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Shop shop = (Shop) o;
+    return id == shop.id &&
+        Objects.equals(name, shop.name) &&
+        Objects.equals(address, shop.address);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, address);
+  }
+
+  @Override
+  public String toString() {
+    return "Shop{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", address='" + address + '\'' +
+        '}';
+  }
+}
