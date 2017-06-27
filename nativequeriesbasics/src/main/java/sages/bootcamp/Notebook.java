441f7c53365732ec8255a4c56562968926f6a9d3
Marcin Król <marcins1024@gmail.com>
2017-06-27 12:01:40 +0200

    Init native-queries-basics

---
+++ nativequeriesbasics/src/main/java/sages/bootcamp/Notebook.java
@@ -0,0 +1,61 @@
+package sages.bootcamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "notebooks")
+public class Notebook {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  private final String model;
+  private final int resolution;
+  private final int sizeInInches;
+  private final String colour;
+  private final Instant productionTimestamp;
+
+  public Notebook(String model, int resolution, int sizeInInches, String colour, Instant productionTimestamp) {
+    this.id = 0;
+    this.model = model;
+    this.resolution = resolution;
+    this.sizeInInches = sizeInInches;
+    this.colour = colour;
+    this.productionTimestamp = productionTimestamp;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public int getResolution() {
+    return resolution;
+  }
+
+  public int getSizeInInches() {
+    return sizeInInches;
+  }
+
+  public String getColour() {
+    return colour;
+  }
+
+  public Instant getProductionTimestamp() {
+    return productionTimestamp;
+  }
+
+  @Override
+  public String toString() {
+    return "Notebook{" +
+        "id=" + id +
+        ", model='" + model + '\'' +
+        ", resolution=" + resolution +
+        ", productionTimestamp=" + productionTimestamp +
+        '}';
+  }
+}
